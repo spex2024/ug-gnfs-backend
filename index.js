ff12a0b6f0986beb18fd9601a7ab909d64863e69
spex2024 <dercolbags@gmail.com>
2025-04-27 08:01:06 +0000

    v1.0.0.0

--- index.js
+++ index.js
@@ -45,13 +45,13 @@ app.use('/api/admin' , adminRoutes)
 
 
 
-
+connectDB()
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  connectDB()
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+ 
 });
